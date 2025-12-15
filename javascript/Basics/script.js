e1ff293571e9bff32f93a8a46adf39aa3eabf2b1
Muhammad Jazib <mjxdex@gmail.com>
2025-12-15 12:17:13 +0500

    js basics

---
+++ javascript/Basics/script.js
@@ -0,0 +1,47 @@
+// Basics
+
+// 0: Keywords and words
+// Woh words jinko js janti ha wo keywords hotay hain usky ilawa saray words kahlatay hain. keywords example: function, let, const, var, async, await, fetch...
+
+// 1: Data Types
+// string: "iohdidsfhoids"
+// number: 12232131
+// Arrays: []
+// Object: {}
+// functions
+// Booleans: true / false
+// undefined
+// null
+
+// 2: Variables
+// # var can redaclare and reassign.
+// var dulha = "shoaib";
+// var dulha = "shahzaib";
+// var dulan = "justifyContent";
+// # let can't redaclare but reassign.
+// let dulha = "grid";
+// dulha = "shahzaib";
+// let dulhan = "flex";
+// # const can't redaclare or reassign.
+// const dulha = "block";
+// dulha = "shahzaib";
+// const dulhan = "flex";
+
+// # var is function scope.
+// var num = 2;
+// function name() {
+//     var num = 40;
+// }
+// console.log(num);
+
+// # let and const is block scope
+// let num = 4;
+// {
+//     let num = 90;
+//     console.log(num);
+//     {
+//         let num = 30;
+//         console.log(num);
+//     }
+// }
+// console.log(num);
