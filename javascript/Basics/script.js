022f792158ea68852330a038cbcca8f619f08ebf
Muhammad Jazib <mjxdex@gmail.com>
2025-12-16 22:33:44 +0500

    types and operators in js

--- javascript/Basics/script.js
+++ javascript/Basics/script.js
@@ -45,3 +45,38 @@
 //     }
 // }
 // console.log(num);
+// var cup;
+// console.log(cup);
+// cup = "coffee";
+// variable which is created with var keyword can hoist. its mean the initialize of the var will lift and the assignment will remain there.
+// let cup = "coffee";
+// const cup = "coffee";
+// let and const can hoist but if we call them before initialize its called TDZ (temporal dead zone)
+
+// Data types:
+// 0-primitives and reference ( without brackets all the types is premetives... )
+// 1-dynamic typing ( there's no static typing in js which mean we don't define type of variable while declare )
+// 2-typeof quirks ( null type is object, nan type is number ) (behaviour)
+// 3-type coercion ( "5"+1 = "51" )(if operand is a string then it will concatenate while using + but calculate if - )( force )
+// 4-truthy vs falsy ( false values... 0 false "" null undefined NaN document.all )(for checking !! write this before value)(nature)(-1 its truthy)
+
+// Operators:
+// 0-types
+// a: artihimetic (+, -, *, %, /)
+// b: assignment (+=, -=, *=)
+// c: logical (&&, ||, !)
+// d: comparison (==, ===, >, <, >=, <=)
+// b: terinay ( ? : )
+
+// const input = Number(prompt("Enter your age"));
+// const age = input >= 18 ? "Eligible" : "Under-age";
+// alert(age);
+
+// 1- pre / post increment
+// let counter = 5;
+// console.log(counter++); post
+// console.log(++counter); pre
+
+// 2-for checking real type of array or object use var instanceof Array / false or true
+// const arr = [];
+// console.log(arr instanceof Array);
