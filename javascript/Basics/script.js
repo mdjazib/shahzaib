24d9e72008ce5ac01ffa9b945824d4e29c95455d
Muhammad Jazib <mjxdex@gmail.com>
2025-12-17 12:27:01 +0500

    js functions

--- javascript/Basics/script.js
+++ javascript/Basics/script.js
@@ -80,3 +80,114 @@
 // 2-for checking real type of array or object use var instanceof Array / false or true
 // const arr = [];
 // console.log(arr instanceof Array);
+
+// Control Flow:
+// 0-if else else if
+
+// const age = Number(prompt("Enter your age:"));
+// if (age >= 18 && typeof age === 'number') {
+//     console.log("License");
+// } else {
+//     console.log("Student card");
+// }
+
+// const fan = !true;
+// if (fan && typeof fan === "boolean") {
+//     console.log("Fan is on.");
+// } else if (!fan && typeof fan === "boolean") {
+//     console.log("Fan is off.");
+// } else {
+//     console.log("Unexpected input value.");
+// }
+
+// 1-switch case
+
+// switch (3) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     default:
+//         console.log("Not a valid number for week.");
+//         break;
+// }
+
+// const age = 18;
+// console.log(age >= 18 ? "Yes" : "No");
+
+// 2-early return pattern
+
+// function button(state) {
+//     return state === true;
+// }
+
+// Loops
+// 0- for
+// let myclass = 46;
+// for (let i = 0; myclass >= i; i++) {
+//     console.log(`Welcome new student: ${i}`);
+// }
+
+// const table = 9;
+// for (let c = 1; 10 >= c; c++) {
+//     console.log(`${table} x ${c} = ${table * c}`);
+// }
+
+// 1- while
+// let num = 1;
+// while (num <= 10000000) {
+//     console.log(num);
+//     num++
+// }
+
+// 2- do-while
+// let num = 0;
+// do {
+//     console.log(num);
+//     num--
+// } while (num >= 1);
+
+// 3- break or continue
+// for (let index = 1; index <= 20; index++) {
+//     // if(index === 3) continue;
+//     // if (index === 11) break;
+//     console.log(index);
+// }
+
+// functions
+
+function shoaib(time) {
+    if (time === 12) console.log("shoaib raat ko 12 bjy code k puchy ga");
+}
+
+shoaib(11);
+
+function sum(...str) { return str.sort() }
+console.log(sum("b", "c", "a", "z", "x"));
+
+function date(d = new Date().toLocaleDateString()) {
+    console.log(d);
+}
+
+date("18/12/2025");
+date();
+
+
+// const bulb = () => {
+//     console.log("fat arrow function");
+// }
+
+// bulb();
+
+const newfn = function () { return 54 };
+console.log(newfn());
+
+
