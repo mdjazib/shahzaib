cf7df1dfbfaf9243b53ee5ff63f412c6edf8d9d0
Muhammad Jazib <mjxdex@gmail.com>
2025-12-23 11:24:04 +0500

    DOM

---
+++ javascript/DOM/script.js
@@ -0,0 +1,27 @@
+// console.log(document.getElementsByClassName("one")[0]);
+// console.log(document.getElementsByTagName("script"));
+// console.log(document.querySelector("#two"));
+
+
+function button(state) {
+    if (state) {
+        alert("Bulb is on.");
+    } else {
+        alert("Bulb is off.");
+    }
+
+    // state ? console.log("Bulb is on.") : console.log("Bulb is off.")
+}
+
+function changeMyColor(element) {
+    element.style.background = "pink";
+}
+
+const box = document.querySelector(".box");
+box.addEventListener("mouseover", () => {
+    box.style.borderRadius = "100%";
+})
+
+box.addEventListener("mouseleave", () => {
+    box.style.borderRadius = "0%";
+})
